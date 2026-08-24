dd2c87ea0c7645e6da7705117bf4f6472f48f94f
Jason Williams <jaswilli@users.noreply.github.com>
2026-08-24 17:48:39 -0600

    Migrate off mocha context (#87)

--- test/integration/support.js
+++ test/integration/support.js
@@ -1,14 +1,15 @@
 import { beforeEach, afterEach } from 'mocha';
 import Support from '../support.js';
 
-beforeEach(function () {
-  this.sequelize.test.trackRunningQueries();
-  return Support.clearDatabase(this.sequelize);
+beforeEach(() => {
+  Support.sequelize.test.trackRunningQueries();
+  return Support.clearDatabase(Support.sequelize);
 });
 
+// Stays a `function` so `this.currentTest` still resolves to the mocha context.
 afterEach(function () {
   try {
-    this.sequelize.test.verifyNoRunningQueries();
+    Support.sequelize.test.verifyNoRunningQueries();
   } catch (err) {
     err.message += ' in ' + this.currentTest.fullTitle();
     throw err;
